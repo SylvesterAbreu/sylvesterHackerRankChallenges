742adf57ddd9f081693ba67da12d795ef297d826
Sylvester Abreu <sylvester.abreu@digitaslbi.com>
2016-06-10 20:30:46 +0100

    Return null when deleting a null linked list.

---
+++ src/main/java/net/sylvestertech/java/easy/dataStructures/LinkedListDelete.java
@@ -0,0 +1,8 @@
+package net.sylvestertech.java.easy.dataStructures;
+
+public class LinkedListDelete {
+
+    public static Node nodeAtPosition(Node head, int position) {
+        return null;
+    }
+}
