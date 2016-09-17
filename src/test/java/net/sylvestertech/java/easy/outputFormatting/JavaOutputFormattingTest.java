4a42993ac3de582d769b1ac8c840e0a814cdf018
Sylvester Abreu <sylvester.abreu@digitaslbi.com>
2016-09-17 21:31:03 +0100

    Refactoring unit tests by extracting common functionality to setup method

--- src/test/java/net/sylvestertech/java/easy/outputFormatting/JavaOutputFormattingTest.java
+++ src/test/java/net/sylvestertech/java/easy/outputFormatting/JavaOutputFormattingTest.java
@@ -4,24 +4,27 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.ByteArrayOutputStream;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class JavaOutputFormattingTest {
 
+    private JavaOutputFormatter javaOutputFormatter;
+
+    @Before
+    public void setup() throws Exception {
+        javaOutputFormatter = new JavaOutputFormatter();
+    }
+
     @Test
     public void printOutFifteenCharactersOfStringAndThreeDigitsWhenTwoDigitsArePassedAsSecondArgument() throws Exception {
-
-        final JavaOutputFormatter javaOutputFormatter = new JavaOutputFormatter();
         final ByteArrayOutputStream outContent = javaOutputFormatter.printTextAndIntegerWithThreeDigits("Hello", 60);
-
         assertEquals("Hello          060", outContent.toString());
     }
 
     @Test
     public void printOutFifteenCharactersOfStringAndThreeDigitsWhenOneDigitIsPassedAsSecondArgument() throws Exception {
-        final JavaOutputFormatter javaOutputFormatter = new JavaOutputFormatter();
         final ByteArrayOutputStream outContent = javaOutputFormatter.printTextAndIntegerWithThreeDigits("Ola", 4);
-
         assertEquals("Ola            004", outContent.toString());
     }
 }
