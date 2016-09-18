de0d420dc962dea8547c6cf2ef098415a7d16683
Sylvester Abreu <sylvester.abreu@digitaslbi.com>
2016-09-18 12:11:06 +0100

    Asserting value A is greater than zero

--- src/main/java/net/sylvestertech/java/easy/loops/JavaLoopsTwo.java
+++ src/main/java/net/sylvestertech/java/easy/loops/JavaLoopsTwo.java
@@ -3,12 +3,18 @@ package net.sylvestertech.java.easy.loops;
 public class JavaLoopsTwo {
 
     private final int queryValue;
+    private final int valueA;
 
-    public JavaLoopsTwo(int queryValue) {
+    public JavaLoopsTwo(int queryValue, int valueA) {
         this.queryValue = queryValue;
+        this.valueA = valueA;
     }
 
     public int getQueryValue() {
         return queryValue >= 0 ? queryValue : -1;
+    }
+
+    public int getValueA() {
+        return valueA;
     }
 }
--- src/test/java/net/sylvestertech/java/easy/loops/JavaLoopsTwoTest.java
+++ src/test/java/net/sylvestertech/java/easy/loops/JavaLoopsTwoTest.java
@@ -8,19 +8,25 @@ public class JavaLoopsTwoTest {
 
     @Test
     public void queryValueIsGreaterThanZero() throws Exception {
-        final JavaLoopsTwo javaLoopsTwo = new JavaLoopsTwo(4);
+        final JavaLoopsTwo javaLoopsTwo = new JavaLoopsTwo(4, 4);
         assertEquals(4, javaLoopsTwo.getQueryValue());
     }
 
     @Test
     public void queryValueIsEqualToZero() throws Exception {
-        final JavaLoopsTwo javaLoopsTwo = new JavaLoopsTwo(0);
+        final JavaLoopsTwo javaLoopsTwo = new JavaLoopsTwo(0, 4);
         assertEquals(0, javaLoopsTwo.getQueryValue());
     }
 
     @Test
     public void queryValueIsMinusOneWhenArgumentPassedIsLessThanZero() throws Exception {
-        final JavaLoopsTwo javaLoopsTwo = new JavaLoopsTwo(-2);
+        final JavaLoopsTwo javaLoopsTwo = new JavaLoopsTwo(-2, 4);
         assertEquals(-1, javaLoopsTwo.getQueryValue());
+    }
+
+    @Test
+    public void assertValueAisGreaterThanZero() throws Exception {
+        final JavaLoopsTwo javaLoopsTwo = new JavaLoopsTwo(1,4);
+        assertEquals(4, javaLoopsTwo.getValueA());
     }
 }
